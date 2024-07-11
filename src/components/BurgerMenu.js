780af9698a32a5c377eae5389cfbd82733978c45
Osvaldo Morales <osvaldom@protonmail.com>
2024-07-11 02:07:46 -0400

    Osvaldo : updated burger menu styling

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Box,
   IconButton,
@@ -15,28 +16,34 @@ const MenuContainer = styled(Box)(({ theme }) => ({
   top: 0,
   left: 0,
   padding: theme.spacing(2),
+  paddingLeft: theme.spacing(4),
   zIndex: 1000,
 }));
 
 const StyledIconButton = styled(IconButton)(({ theme }) => ({
-  fontSize: "2rem", // Increase icon size
+  fontSize: "2rem",
+}));
+
+const MenuContent = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  top: "100%",
+  left: theme.spacing(4),
 }));
 
 const StyledList = styled(List)(({ theme }) => ({
-  backgroundColor: "#B6CB9E", // Changed background color
+  backgroundColor: "#B6CB9E",
   borderRadius: theme.shape.borderRadius,
   boxShadow: theme.shadows[4],
-  marginTop: theme.spacing(1),
-  width: 250, // Increased width
+  width: 250,
 }));
 
 const StyledListItem = styled(ListItem)(({ theme }) => ({
-  padding: theme.spacing(2), // Increase padding for larger menu items
+  padding: theme.spacing(2),
 }));
 
 const StyledListItemText = styled(ListItemText)(({ theme }) => ({
   "& .MuiListItemText-primary": {
-    fontSize: "1.2rem", // Increase font size
+    fontSize: "1.2rem",
   },
 }));
 
@@ -47,7 +54,10 @@ function BurgerMenu() {
     setIsOpen(!isOpen);
   };
 
-  const menuItems = ["Home", "About", "Contact", "Projects"];
+  const menuItems = [
+    { text: "Home", path: "/" },
+    { text: "Timer", path: "/timer" },
+  ];
 
   return (
     <MenuContainer>
@@ -60,15 +70,22 @@ function BurgerMenu() {
       >
         <MenuIcon fontSize="inherit" />
       </StyledIconButton>
-      <Collapse in={isOpen} timeout="auto" unmountOnExit>
-        <StyledList>
-          {menuItems.map((text) => (
-            <StyledListItem button key={text}>
-              <StyledListItemText primary={text} />
-            </StyledListItem>
-          ))}
-        </StyledList>
-      </Collapse>
+      <MenuContent>
+        <Collapse in={isOpen} timeout="auto" unmountOnExit>
+          <StyledList>
+            {menuItems.map((item) => (
+              <StyledListItem
+                button
+                key={item.text}
+                to={item.path}
+                onClick={toggleMenu}
+              >
+                <StyledListItemText primary={item.text} />
+              </StyledListItem>
+            ))}
+          </StyledList>
+        </Collapse>
+      </MenuContent>
     </MenuContainer>
   );
 }
