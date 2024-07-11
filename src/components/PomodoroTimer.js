c94ba784685bf2fcd18bada571221b369d8ab3ec
Osvaldo Morales <osvaldom@protonmail.com>
2024-07-11 16:16:53 -0400

    Osvaldo: added reset button to pomodor timer

--- src/components/PomodoroTimer.js
+++ src/components/PomodoroTimer.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Box, Button, Typography } from "@mui/material";
 
 const PomodoroTimer = () => {
-  const [timer, setTimer] = useState(1500); // 25 minutes
+  const initialTime = 1500; // 25 minutes
+  const [timer, setTimer] = useState(initialTime);
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
@@ -21,6 +22,11 @@ const PomodoroTimer = () => {
     setIsActive(!isActive);
   };
 
+  const resetTimer = () => {
+    setIsActive(false);
+    setTimer(initialTime);
+  };
+
   const formatTime = (seconds) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
@@ -37,8 +43,11 @@ const PomodoroTimer = () => {
       <Typography variant="body1" color={"#81667A"} gutterBottom>
         {formatTime(timer)}
       </Typography>
-      <Button variant="contained" color={"secondary"} onClick={toggleTimer}>
+      <Button variant="contained" color={"secondary"} onClick={toggleTimer} style={{ marginRight: '10px' }}>
         {isActive ? "Pause" : "Start"}
+      </Button>
+      <Button variant="contained" color={"secondary"} onClick={resetTimer}>
+        Reset
       </Button>
     </Box>
   );
